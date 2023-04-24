773fcb1a9b8281b1841888673b6d16ba9e67129c
Dmitry Sorokin <soradimichi@gmail.com>
2023-04-24 21:08:27 +0400

    finished

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { NEWS } from "./consts.js";
 import { BaseData, Item } from "./types.js";
 const PATH_TO_BASE = "build/base.json";
 
-const writeObjectToJSON = async (
+const writeToBase = async (
   data: Item[],
   path = PATH_TO_BASE,
   encode = "utf-8" as const
@@ -15,11 +15,11 @@ const writeObjectToJSON = async (
     throw new Error(`Base in ${path} not found`);
   }
 
-  const newBase = data.reduce((acc, { name, issue }) => ({ ...acc, [name]: issue }), {});
+  const newBase = {...base, ...data.reduce((acc, { name, issue }) => ({ ...acc, [name]: issue }), {})};
   await writeFile(path, JSON.stringify(newBase, null, 2), encode);
 };
 
-const setIssue = (base: BaseData, initial: Item[] = NEWS) =>
+const syncWithBase = (base: BaseData, initial: Item[] = NEWS) =>
   initial.reduce((acc: Item[], item: Item) => {
     const key = item.name;
     if (!(key in base)) throw new Error(`Key ${key} not found in base`);
@@ -27,14 +27,16 @@ const setIssue = (base: BaseData, initial: Item[] = NEWS) =>
     return [...acc, { ...item, issue: base[key] }];
   }, []);
 
-const checkURL = ({ url, issue, ...p }: Item) => {
+const checkURL = async ({ url, issue, ...p }: Item) => {
   const newIssue = issue + 1;
   const newUrl = url + newIssue;
-  return fetch(newUrl)
-    .then(({status}) => (status === 200 ? { ...p, url: newUrl, issue: newIssue, updated: true } : { ...p, url, issue, updated: false }));
+  const {status} = await fetch(newUrl);
+  return { ...p, url: newUrl, issue: newIssue, updated: status === 200 }
 };
 
-const sendToDiscord = ({ webhook, url, ...p }: Item) =>
+const fetchIssues = () => Promise.all(syncWithBase(base).map(checkURL))
+
+const postToDiscord = ({ webhook, url, ...p }: Item) =>
   fetch(webhook, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -42,8 +44,11 @@ const sendToDiscord = ({ webhook, url, ...p }: Item) =>
   })
     .then(({status}) =>  ({ ...p, webhook, url, published: status === 200 }))
 
-const runner = () => Promise.all(setIssue(base).map(checkURL))
-    .then(async (data) => ([...data, ...await Promise.all(data.filter(({ updated }: Item) => updated).map(sendToDiscord))]))
-    .then(writeObjectToJSON).catch(console.error);
+const postIssues = (data: Item[]) => Promise.all(data.filter(({ updated }: Item) => updated).map(postToDiscord))
+
+const runner = () => fetchIssues()
+    .then(postIssues)
+    .then(writeToBase)
+    .catch(console.error);
 
 runner();
